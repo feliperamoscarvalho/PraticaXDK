b053d2f4c44e53835ec6dc47fa59bd1bb4389b4d
Felipe <ramos.felippe@gmail.com>
2017-05-28 19:27:20 -0300

    Organização do código

--- www/js/banco_dados.js
+++ www/js/banco_dados.js
@@ -1,10 +1,12 @@
 $(function(){
 	var i = Number(localStorage.getItem('paciente-contador')) + 1;
-	var j, k, orderList;
-	var $paciente = $("#NomePacientes");
-    var $pacienteIdade = $("#IdadePacientes");
-    var $pacienteEmail = $("#EmailPacientes");
-	var $pacienteList = $("#pacientes");
+	var j;
+    var k;
+    var orderList;
+	var $paciente = $("#NomePacientes"); //Variável com o valor digitado para o nome do Paciente
+    var $pacienteIdade = $("#IdadePacientes"); //Variável com o valor digitado para a idade do Paciente
+    var $pacienteEmail = $("#EmailPacientes"); //Variável com o valor digitado para o email do Paciente
+	var $pacienteList = $("#pacientes"); //lista de pacientes
 	var order = [];
 	orderList = localStorage.getItem('paciente-cadastro');
 	
@@ -13,31 +15,31 @@ $(function(){
 	}
 
 	
-		// Mostrar Lista de Pacientes		
-		orderList = orderList ? orderList.split(',') : [];   
-		for( j = 0, k = orderList.length; j < k; j++) {
-			$pacienteList.append(
-				"<li id='" + orderList[j] + "'>"
-				+ "<a class='editable' data-split-theme='c'>"	
-				+ localStorage.getItem(orderList[j]) 
-				+ "</a> <a href='#' class='close' data-icon='delete' data-theme='c'>X</a></li>"
-			);
-		}
+    // Mostrar Lista de Pacientes		
+    orderList = orderList ? orderList.split(',') : [];   
+    for( j = 0, k = orderList.length; j < k; j++) {
+        $pacienteList.append(
+            "<li id='" + orderList[j] + "'>"
+            + "<a class='editable' data-split-theme='c'>"	
+            + localStorage.getItem(orderList[j]) 
+            + "</a> <a href='#' class='close' data-icon='delete' data-theme='c'>X</a></li>"
+        );
+    }
     
 	// Incluir Pacientes 
 	$("#IncluirPacientes").live("tap", function() {
 		if($paciente.val() != ""){
 			localStorage.setItem("paciente-"+i, $paciente.val());
-            localStorage.setItem("pacienteIdade-"+i, $pacienteIdade.val());//novo
-            localStorage.setItem("pacienteEmail-"+i, $pacienteEmail.val());//novo
+            localStorage.setItem("pacienteIdade-"+i, $pacienteIdade.val());
+            localStorage.setItem("pacienteEmail-"+i, $pacienteEmail.val());
 			localStorage.setItem("paciente-contador",i);
 			$("#semErros").css("display","none");
 			$pacienteList.append(
 				"<li id='paciente-" + i + "'>" 
 				+  "<a class='editable' data-split-theme='c'>" 
 				+ "Nome: " + localStorage.getItem("paciente-" + i) + " / "
-                + "Idade: " + localStorage.getItem("pacienteIdade-" + i) + " / "//novo
-                + "E-mail: " + localStorage.getItem("pacienteEmail-" + i)//novo
+                + "Idade: " + localStorage.getItem("pacienteIdade-" + i) + " / "
+                + "E-mail: " + localStorage.getItem("pacienteEmail-" + i)
 				+ " </a><a href='#' data-icon='delete' class='close' data-theme='c'>x</a></li>"
 			);
 			$.mobile.changePage("#ListaPacientes", { transition: "slidedown"});		
